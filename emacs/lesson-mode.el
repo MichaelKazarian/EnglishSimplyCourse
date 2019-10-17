bf857dc4a9a5e573bb8fd503882f63348d29d71b
kazarian <michael.kazarian@gmail.com>
2019-10-17 12:07:06 +0300

    lesson-mode.el was improved

--- emacs/lesson-mode.el
+++ emacs/lesson-mode.el
@@ -1,4 +1,4 @@
-;;; my-lesson-org-mode.el --- 
+;;; lesson-org-mode.el --- 
 
 ;; Copyright (C) Michael Kazarian
 ;;
@@ -12,18 +12,32 @@
 ;;
 ;; Customizable Variables
 ;;
+(defconst lesson-mode-version "0.1"
+  "The version of `lesson-mode'.")
 
-(defconst my-lesson-mode-version "0.1"
-  "The version of `my-lesson-mode'.")
-
-(defcustom my-message-time-delay 6
+(defcustom message-time-delay 6
   "Delay time before answer hint."
   :type 'integer
   :safe 'integerp
   :group 'lesson-mode)
 
+(defcustom word-preview-time 3
+  "Word preview time"
+  :type 'integer
+  :safe 'integerp
+  :group 'lesson-mode)
+
+(defcustom answer-question-delimiter ";"
+  "Question-answer delimiter. Answer is first."
+  :type 'string
+  :group 'lesson-mode)
+
+;;
+;; Core
+;;
 (defun lang-delimiter-position (str)
-  (string-match ";" str))
+  "Returns `answer-question-delimiter' position if presents; nil otherwise"
+  (string-match answer-question-delimiter str))
 
 (defun answer-message ()
   "Hint to say answer and publish it in the slide"
@@ -46,7 +60,7 @@
 
 (defun lesson-switch-to-lesson ()
   "Switch to lesson window. After switching searche next question by
-number. If question found send message after `my-message-time-delay' sec."
+number. If question found send message after `message-time-delay' sec."
   (interactive)
   (other-window 1)
   (let ((start-point (re-search-forward "^[[:space:]]*[0-9]" nil t)))
@@ -54,7 +68,7 @@ number. If question found send message after `my-message-time-delay' sec."
         (progn
           (goto-char start-point)
           ;; (new-q-message)
-          (run-at-time my-message-time-delay nil #'answer-message))
+          (run-at-time message-time-delay nil #'answer-message))
       (next-line))))
 
 (defun lesson-send-to-slide ()
@@ -79,16 +93,22 @@ number. If question found send message after `my-message-time-delay' sec."
   (lesson-switch-to-lesson)
   (next-window 1))
 
-(defun word-send-to-slide ()
-  "Send current word to slide buffer and switch to it"
+(defun word-region-blink-to-slide ()
+  "Temporary send current word or region to slide buffer and switch to it.
+Clear this line after `word-preview-time and switch to previous buffer."
   (interactive)
-  (let* ((str (word-at-point)))
+  (let ((str (word-at-point)))
+    (if (region-active-p)
+            (setq str (buffer-substring (region-beginning) (region-end))))
     (with-current-buffer "slide"
       (progn
-      (insert (concat "\n" str))
-      (end-of-line))))
-  (lesson-switch-to-lesson)
-  (next-window 1))
+        (message (region-active-p))
+        (insert (concat "\n~" (string-trim str) "~"))
+        (end-of-line)
+        (sit-for word-preview-time)
+        (clear-line-go-to-lesson)
+        (other-window 1)
+        ))))
 
 (defun clear-line-go-to-lesson ()
   "Clear current line and return to previous window"
@@ -115,81 +135,14 @@ If slide buffer is active switch to lesson buffer an search new question"
       (progn
         (generate-new-buffer slide-name)
         (with-current-buffer slide-name
-          (funcall 'my-lesson-mode)
+          (funcall 'lesson-mode)
           (text-scale-set 3)
+          (split-window-right)
+          (switch-to-buffer-other-window slide-name)
+          (enlarge-window-horizontally 35)
+          (other-window 1)
           ;(text-scale-adjust 1)
-          )
-        (split-window-right)
-        (switch-to-buffer-other-window slide-name)
-        (enlarge-window-horizontally 35)))))
-
-(defun lesson-to-json ()
-  (interactive)
-  (let ((from-file (read-file-name "To json:" (buffer-file-name)))
-         (to-file (read-file-name "Save to:" nil nil nil (concat (file-name-sans-extension (buffer-name)) ".json"))))
-         (message to-file)
-         (setq input-lines (read-json from-file))
-         (write-json input-lines to-file)
-         (find-file to-file)
-         ))
-
-(defun read-json (file)
-  (split-string
-          (with-temp-buffer
-           (insert-file-contents file)
-           (buffer-substring-no-properties
-            (point-min)
-            (point-max)))
-          "\n" t))
-
-(defun write-json (lines file)
-  (setq header "{
-  \"lessonId\": \"lesson\",
-  \"lessonTitle\": \"Урок \",
-  \"lessonDescription\": \" \",
-  \"lessonURL\": \"bJkSM3g-Tfs\",
-  \"lessonItems\": [")
-  (setq footer "\n  ]\n}\n")
-  (setq point "\n    {
-      \"itemType\": \"point\",
-      \"itemText\": \"%s\",
-      \"startTime\": \":\"
-    },")
-  (setq question "\n    {
-      \"itemType\": \"question\",
-      \"question\": \"\",
-      \"answer\":   \"%s\",
-      \"startTime\": :
-    },")
-  (with-temp-file file
-    (insert header)
-    (setq item-type point)
-    (dolist (line lines)
-      (if (string-match "^[[:space:]]*[[:digit:]]\\{1,5\\}\\. " line)
-          (setq item-type question))
-      (insert (format
-               item-type
-               (replace-org-to-html line))))
-    (insert footer))
-  )
-
-(defun replace-org-to-html (str)
-  (let* ((pattern "[/_\\*]\\(.+\\)[/_\\*]")
-         (found (string-match pattern str)))
-    (if (null found)
-        str
-      (progn
-        (setq tag  (substring str found (+ found 1)))
-        (message tag)
-        (setq t-s "") (setq t-e "")
-        (cond ((equal "*" tag) (setq t-s "<b>") (setq t-e "</b>"))
-              ((equal "_" tag) (setq t-s "<u>") (setq t-e "</u>"))
-              ((equal "/" tag) (setq t-s "<i>") (setq t-e "</i>")))
-        (mapconcat
-         'identity (split-string str pattern)
-         (concat t-s (match-string 1 str) t-e)))
-      ))
-  )
+          )))))
 
 (defun s ()
   "Setup `org-mode' for lesson"
@@ -199,53 +152,160 @@ If slide buffer is active switch to lesson buffer an search new question"
   (olivetti-mode -1)
   (setq org-hide-emphasis-markers t)
   (load-theme 'dichromacy t)
+
   ;; make part of a word bold
   ;; https://stackoverflow.com/posts/24540651/revisions
   (setcar org-emphasis-regexp-components " \t('\"{[:alpha:]")
   (setcar (nthcdr 1 org-emphasis-regexp-components) "[:alpha:]- \t.,:!?;'\")}\\")
   (org-set-emph-re 'org-emphasis-regexp-components org-emphasis-regexp-components)
   ;;
-  (set-frame-size (selected-frame) 150 60))
-
-(defun my-lesson-mode-version ()
-  "Show the `my-lesson-mode' version in the echo area."
+  (set-frame-size (selected-frame) 150 60)
+  (set-face-attribute 'org-level-2 nil :foreground "DarkGreen")
+  (add-to-list 'org-emphasis-alist
+             '("~" (:foreground "OrangeRed"))))
+;;
+;; orgl to json converter
+;;
+(defun lesson-to-json ()
+  "Convert lesson to json. If line begining is numbered item line will convert
+to json question item (use ; to split question and answer). All other strings
+will convert to json point type."
   (interactive)
-  (message (concat "lesson-mode version " my-lesson-mode-version)))
+  (let ((from-file (read-file-name "To json:" (buffer-file-name)))
+         (to-file (read-file-name "Save to:" nil nil nil (concat (file-name-sans-extension (buffer-name)) ".json"))))
+         (message to-file)
+         (setq input-lines (read-lesson from-file))
+         (write-json input-lines to-file)
+         (find-file to-file)))
 
-(defvar my-lesson-mode-map
+(defun read-lesson (file)
+  "Read `file'.
+Returns list of strings"
+  (split-string
+          (with-temp-buffer
+           (insert-file-contents file)
+           (buffer-substring-no-properties
+            (point-min)
+            (point-max)))
+          "\n" t))
+
+(defun write-json (lines file)
+  "Writes `lines' to `file'"
+  (setq header "{
+  \"lessonId\": \"lesson\",
+  \"lessonTitle\": \"Урок \",
+  \"lessonDescription\": \" \",
+  \"lessonURL\": \"bJkSM3g-Tfs\",
+  \"lessonItems\": [")
+  (setq footer "\n  ]\n}\n")
+  (with-temp-file file
+    (insert header)
+    (dolist (line lines)
+      (setq l (replace-org-to-html line))
+      (if (string-match "^[[:space:]]*[[:digit:]]\\{1,5\\}\\. " l)
+          (setq item (get-question l))
+        (setq item (get-point l)))
+      (insert item))
+    (insert footer)))
+
+(defun get-question (str)
+  "Returns prepared json question item. If `str' contains
+`answer-question-delimiter' first part will answer, the second one will
+question. If delimiter omited question part will empty"
+  (setq template "\n    {
+      \"itemType\": \"question\",
+      \"question\": \"%s\",
+      \"answer\":   \"%s\",
+      \"startTime\": \":\"
+    },")
+  (format template
+   (get-question-part str)
+   (get-answer-part str)))
+
+(defun get-point (str)
+  "Returns prepared json point item"
+  (setq template "\n    {
+      \"itemType\": \"point\",
+      \"itemText\": \"%s\",
+      \"startTime\": \":\"
+    },")
+  (format template str))
+
+(defun replace-org-to-html (str)
+  "Relaces /*_ org formating to <i></i>, <b></b>, <u></u> accordingly"
+  (let* ((pattern "[/_\\*]\\(.+\\)[/_\\*]")
+         (found (string-match pattern str)))
+    (if (null found)
+        str
+      (progn
+        (setq tag (substring str found (+ found 1)))
+        (message tag)
+        (setq t-s "") (setq t-e "")
+        (cond ((equal "*" tag) (setq t-s "<b>") (setq t-e "</b>"))
+              ((equal "_" tag) (setq t-s "<u>") (setq t-e "</u>"))
+              ((equal "/" tag) (setq t-s "<i>") (setq t-e "</i>")))
+        (mapconcat
+         'identity (split-string str pattern)
+         (concat t-s (match-string 1 str) t-e)))
+      )))
+
+(defun get-answer-part (line)
+  "If `line' contains `answer-question-delimiter' first part will return.
+Whole line otherwise"
+  (let ((delimiter (lang-delimiter-position line)))
+    (if (null delimiter)
+        line
+      (string-trim (substring line 0 delimiter)))))
+
+(defun get-question-part (line)
+  "If `line' contains `answer-question-delimiter' the second part will return.
+Empty line otherwise"
+  (let ((delimiter (lang-delimiter-position line)))
+    (if (null delimiter)
+        ""
+      (string-trim (substring line (+ 1 delimiter))))))
+;;
+;; Mode settings, etc
+;;
+(defun lesson-mode-version ()
+  "Show the `lesson-mode' version in the echo area."
+  (interactive)
+  (message (concat "lesson-mode version " lesson-mode-version)))
+
+(defvar lesson-mode-map
   (let ((map (make-sparse-keymap)))
     ;; key bindings
     (define-key map (kbd "<f5>") 'lesson-slide-swich)
-    (define-key map (kbd "<f6>") 'word-send-to-slide)
+    (define-key map (kbd "<f6>") 'word-region-blink-to-slide)
     (define-key map (kbd "<f8>") 'clear-line-go-to-lesson)
     (define-key map (kbd "<f9>") 'lesson-new-slide)
     map)
-  "Keymap for `my-lesson-mode' major mode.")
+  "Keymap for `lesson-mode' major mode.")
 
-(easy-menu-define my-lesson-mode-menu my-lesson-mode-map
-  "Menu for `my-lesson-mode' mode"
+(easy-menu-define lesson-mode-menu lesson-mode-map
+  "Menu for `lesson-mode' mode"
   '("Lesson"
     ["New slide" lesson-new-slide]
     ["Send line to slide" lesson-slide-swich]
-    ["Send word to slide" word-send-to-slide]
+    ["Word or region blink to slide" word-region-blink-to-slide]
     "---"
-    ["Version" my-lesson-mode-version]))
+    ["Version" lesson-mode-version]))
 
-(define-derived-mode my-lesson-mode org-mode "Lesson"
+(define-derived-mode lesson-mode org-mode "Lesson"
   "Major mode for English lesson"
-  (use-local-map my-lesson-mode-map))
+  (use-local-map lesson-mode-map))
 
-(add-hook 'my-lesson-mode-hook 's)
-(add-hook 'my-lesson-mode-hook 'flyspell-mode)
-(provide 'my-lesson-mode)
+(add-hook 'lesson-mode-hook 's)
+(add-hook 'lesson-mode-hook 'flyspell-mode)
+(provide 'lesson-mode)
 
 ;;
 ;; On Load
 ;;
-;; Run my-lesson-mode for files ending in .orgl or .lesson
+;; Run lesson-mode for files ending in .orgl or .lesson
 ;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.lesson\\'" . my-lesson-mode))
-(add-to-list 'auto-mode-alist '("\\.orgl\\'" . my-lesson-mode))
+(add-to-list 'auto-mode-alist '("\\.lesson\\'" . lesson-mode))
+(add-to-list 'auto-mode-alist '("\\.orgl\\'" . lesson-mode))
 
 ;; ;; Old
 ;; (defun lesson-new-slide-temp ()
@@ -258,11 +318,11 @@ If slide buffer is active switch to lesson buffer an search new question"
 ;;           ))
 ;;     (generate-new-buffer slide-name)
 ;;     (with-current-buffer slide-name
-;;       (funcall 'my-lesson-mode))
+;;       (funcall 'lesson-mode))
 ;;     (split-window-right)
 ;;     (switch-to-buffer-other-window slide-name)))
 
-;; (defun my-lesson-clear-order-mark ()
+;; (defun lesson-clear-order-mark ()
 ;;   (interactive)
 ;;   (previous-line)
 ;;   (while (re-search-forward " \\[@[[:digit:]{1,4}]\\] " nil t)
@@ -275,4 +335,4 @@ If slide buffer is active switch to lesson buffer an search new question"
 ;; (with-current-buffer "slide"
 ;;   (insert (concat str "\n")))
 
-;;; my-lesson-org-mode.el ends here
+;;; lesson-org-mode.el ends here
